cd8ffee0de2151ef86b1143fb529f8672f1dcc54
medina <ammedina@inf.ufsm.br>
2021-12-02 18:15:19 -0300

    test: adiciona teste de criação de empresa

--- tests/Feature/BusinessApiTest.php
+++ tests/Feature/BusinessApiTest.php
@@ -3,6 +3,8 @@
 namespace Jetimob\NotaFacil\Tests\Feature;
 
 use Jetimob\NotaFacil\Api\Business\BusinessApi;
+use Jetimob\NotaFacil\Api\Business\DTO\BusinessDTO;
+use Jetimob\NotaFacil\Api\DTO\AddressDTO;
 use Jetimob\NotaFacil\Facades\NotaFacil;
 use Jetimob\NotaFacil\Tests\AbstractTestCase;
 
@@ -19,6 +21,41 @@ class BusinessApiTest extends AbstractTestCase
     public function testAuthenticated(): void
     {
         dump($this->api->authenticated());
+    }
+
+    public function testCreation(): void
+    {
+        try {
+            $res = $this->api->create($this->getBusinessDTO());
+            $this->assertEquals(201, $res->getStatusCode());
+
+            $business = $res->getBusiness();
+            $this->assertIsInt($business->getId());
+            $this->assertIsString($business->getConsumerId());
+            $this->assertNotEquals('', $business->getConsumerId());
+
+            dump($res);
+        } catch (\Throwable $e) {
+            dump($e->getTraceAsString());
+        }
+    }
+
+    protected function getBusinessDTO(): BusinessDTO
+    {
+        return (new BusinessDTO())
+            ->setEnderecoPrincipal(
+                AddressDTO::newWithIbgeCity(
+                    '4316907',
+                    'R Appel',
+                    'Nossa Sra. de Fátima',
+                    '347',
+                    '97015030'
+                )
+            )->setNomeRazaoSocial('ORUS TECNOLOGIA LTDA.')
+            ->setNomeFantasia('Jetimob')
+            ->setCnpjCpf('12544265000130')
+            ->setEmail('contato@jetimob.com');
+
     }
 
     public function testList(): void
